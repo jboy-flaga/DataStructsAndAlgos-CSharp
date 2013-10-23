3abebec3e8e8f00408fb31f6f0655a2c975665b6
Jboy Flaga <jeremiahflaga@gmail.com>
2013-10-23 10:24:31 +0800

    SimpleLinkedList

---
+++ DataStructsAndAlgos.CSharp.Test/SimpleLinkedListTest.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using DataStuctsAndAlgos.CSharp;
+using Xunit;
+
+namespace DataStructsAndAlgos.CSharp.Test
+{
+    public class SimpleLinkedListTest
+    {
+        public class Indexer
+        {
+            [Fact]
+            public void ThrowsIndexOutOfRangeExceptionIfIndexIsNotValid()
+            {
+                SimpleLinkedList<string> list = new SimpleLinkedList<string>();
+
+                Assert.ThrowsDelegate act = delegate { list[777] = "asdf"; };
+
+                Assert.Throws<IndexOutOfRangeException>(act);
+            }
+
+            [Fact]
+            public void ReturnsCorrectItem()
+            {
+                SimpleLinkedList<string> list = new SimpleLinkedList<string>();
+
+                list.Insert("asdf");
+                list.Insert("qwer");
+
+                Assert.True((list[0] as string) == "asdf");
+                Assert.True((list[1] as string) == "qwer");
+            }
+
+            [Fact]
+            public void SetsCorrectItemInList()
+            {
+                SimpleLinkedList<string> list = new SimpleLinkedList<string>();
+
+                list.Insert("asdf");
+                list.Insert("qwer");
+                list.Insert("zxcv");
+
+                list[1] = "aaaa";
+
+                Assert.True((list[0] as string) == "asdf");
+                Assert.True((list[1] as string) != "qwer");
+                Assert.True((list[1] as string) == "aaaa");
+            }
+        }
+
+        public class InsertMethod
+        {
+            [Fact]
+            public void InsertsNewItemInList()
+            {
+                // Arrange
+                SimpleLinkedList<string> list = new SimpleLinkedList<string>();
+                string newItem1 = "new Item 1";
+                string newItem2 = "new Item 2";
+                // Act
+                list.Insert(newItem1);
+                list.Insert(newItem2);
+
+                // Assert
+                Assert.ReferenceEquals((list[0] as string), newItem1);
+                Assert.True((list[0] as string) == "new Item 1");
+                Assert.ReferenceEquals((list[1] as string), newItem2);
+                Assert.True((list[1] as string) == "new Item 2");
+            }
+
+            [Fact]
+            public void IncrementsLengthOfList()
+            {
+                SimpleLinkedList<string> list = new SimpleLinkedList<string>();
+
+                list.Insert("asdf");
+
+                Assert.True(list.Length == 1);
+            }
+        }
+
+        public class RemoveMethod
+        {
+            [Fact]
+            public void RemovesNewItemFromList()
+            {
+                SimpleLinkedList<string> list = new SimpleLinkedList<string>();
+
+                list.Insert("asdf");
+                list.Insert("qwer");
+                list.Remove("asdf");
+
+                Assert.True((list[0] as string) != "asdf");
+            }
+
+            [Fact]
+            public void DecrementsLengthOfList()
+            {
+                SimpleLinkedList<string> list = new SimpleLinkedList<string>();
+
+                list.Insert("asdf");
+                list.Insert("qwer");
+                list.Insert("zxcv");
+                list.Remove("asdf");
+
+                Assert.True(list.Length == 2);
+            }
+
+            [Fact]
+            public void ReturnsNullIfItemIsNotFound()
+            {
+                SimpleLinkedList<string> list = new SimpleLinkedList<string>();
+
+                list.Insert("asdf");
+                list.Insert("qwer");
+                string removedItem = list.Remove("zzzzzzzzzzzzz");
+
+                Assert.Null(removedItem);
+            }
+        }
+
+        public class FirstProperty
+        {
+            [Fact]
+            public void ReturnsFirstItemInList()
+            {
+                SimpleLinkedList<string> list = new SimpleLinkedList<string>();
+
+                list.Insert("asdf");
+                list.Insert("qwer");
+
+                Assert.True(list.First == "asdf");
+            }
+        }
+
+        public class LastProperty
+        {
+            [Fact]
+            public void ReturnsLastItemInList()
+            {
+                SimpleLinkedList<string> list = new SimpleLinkedList<string>();
+
+                list.Insert("asdf");
+                list.Insert("qwer");
+                list.Insert("zxcv");
+
+                Assert.True(list.Last == "zxcv");
+            }
+        }
+    }
+}
---
+++ DataStuctsAndAlgos.CSharp/SimpleLinkedList.cs
@@ -0,0 +1,109 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace DataStuctsAndAlgos.CSharp
+{
+    public class SimpleLinkedList<T>
+    {
+        private class Cell
+        {
+            public T Item;
+            public Cell Link;
+        }
+
+        private Cell _first;
+        private Cell _last;
+        private int _length;
+
+        public int Length
+        {
+            get { return _length; }
+            set { _length = value; }
+        }
+
+        public T First
+        {
+            get { return this._first.Link == null ? default(T) : this._first.Link.Item; }
+        }
+
+        public T Last
+        {
+            get { return this._length == 0 ? default(T) : this[this._length - 1]; }
+        }
+
+        public SimpleLinkedList()
+        {
+            Cell dummyCell = new Cell { Item = default(T), Link = null };
+            this._first = dummyCell;
+            this._last = dummyCell;
+            this._length = 0;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (this._first.Link == null) { throw new IndexOutOfRangeException(); }
+                if (index >= this._length) { throw new IndexOutOfRangeException(); }
+
+                Cell currentCell = this._first.Link;
+                for (int i = 0; i < index; i++)
+                {
+                    currentCell = currentCell.Link;
+                }
+
+                return currentCell.Item;
+            }
+            set
+            {
+                if (this._first.Link == null) { throw new IndexOutOfRangeException(); }
+                if (index >= this._length) { throw new IndexOutOfRangeException(); }
+
+                Cell currentCell = this._first.Link;
+                for (int i = 0; i < index; i++)
+                {
+                    currentCell = currentCell.Link;
+                }
+
+                currentCell.Item = (T)value;
+            }
+        }
+
+        public void Insert(T item)
+        {
+            Cell newCell = new Cell { Item = item, Link = null };
+            this._last.Link = newCell;
+            this._last = newCell;
+            this._length++;
+        }
+
+        /** 
+         * returns null if object to be removed is not found in the list else returns the removed item
+         * 
+         * */
+        public T Remove(T item)
+        {
+            Cell previousCell = this._first;
+            Cell currentCell = this._first.Link;
+            while (currentCell != null)
+            {
+                if (currentCell.Item.Equals(item))
+                {
+                    previousCell.Link = currentCell.Link;
+                    // delete currentCell - currentCell will be garbase collected
+                    this._length--;
+                    return currentCell.Item;
+                }
+
+                Cell nextCell = currentCell.Link;
+                previousCell = currentCell;
+                currentCell = nextCell;
+            }
+
+            return default(T);
+        }
+    }
+}
